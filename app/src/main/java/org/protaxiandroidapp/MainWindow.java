83ec1bdccf8cbef29872b8a78177827fe5a05bc7
dcubas16 <dcubas16@gmail.com>
2016-01-06 17:03:16 -0500

    Adding map

--- app/src/main/java/org/protaxiandroidapp/MainWindow.java
+++ app/src/main/java/org/protaxiandroidapp/MainWindow.java
@@ -3,6 +3,7 @@ package org.protaxiandroidapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -80,17 +81,13 @@ public class MainWindow extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+      if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
+          Log.i("Hola","nav_share");
 
         } else if (id == R.id.nav_send) {
+          Log.i("Hola","nav_share");
 
         }
 
